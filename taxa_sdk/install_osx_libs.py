9b202535d6ea994bd6119b8edc54029d9315c707
Chris Priest <chris@taxa.network>
2022-03-15 10:00:17 -0600

    added signed library install script

---
+++ taxa_sdk/install_osx_libs.py
@@ -0,0 +1,16 @@
+import sys
+import os
+import shutil
+
+source_dir = "/usr/local/lib/apple_signed/"
+
+this_path = os.path.dirname(os.path.abspath(__file__))
+version = sys.argv[1]
+
+if version == '--catalina':
+    lib_path = os.path.normpath(os.path.join(this_path, "bin/libs/OSX/catalina"))
+
+for filename in os.listdir(lib_path):
+    f = os.path.join(lib_path, filename)
+    shutil.copy2(f, source_dir)
+    print("copied", f, "to", source_dir)
