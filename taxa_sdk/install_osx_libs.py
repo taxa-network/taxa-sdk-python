2e50c8cb469abb48d2ca5975f246f94bda78a499
Chris Priest <chris@taxa.network>
2022-03-17 16:59:38 -0600

    added installer biolerplate code for other osx versions in osx lib installer script

--- taxa_sdk/install_osx_libs.py
+++ taxa_sdk/install_osx_libs.py
@@ -8,10 +8,15 @@ this_path = os.path.dirname(os.path.abspath(__file__))
 version = sys.argv[1]
 
 if version == '--catalina':
-    lib_path = os.path.normpath(os.path.join(this_path, "bin/libs/OSX/catalina"))
+    dest = "bin/libs/OSX/catalina"
+if version == '--bigsur':
+    dest = "bin/libs/OSX/bigsur"
+if version == '--monterey':
+    dest = "bin/libs/OSX/monterey"
 
+lib_path = os.path.normpath(os.path.join(this_path, dest))
 os.mkdir(source_dir)
 for filename in os.listdir(lib_path):
-    f = os.path.join(lib_path, filename)
-    shutil.copy2(f, source_dir)
-    print("copied", f, "to", source_dir)
+    full_path = os.path.join(lib_path, filename)
+    shutil.copy2(full_path, source_dir)
+    print("copied", full_path, "to", source_dir)
