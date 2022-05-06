91a607bc2d8fb1787d86cab414e9a8fb2cd95e84
Chris Priest <chris@taxa.network>
2022-05-06 13:10:10 -0600

    osx_install_libs doesn't crash when running install script over again

--- taxa_sdk/install_osx_libs.py
+++ taxa_sdk/install_osx_libs.py
@@ -9,7 +9,9 @@ source_dir = "/usr/local/lib/apple_signed/"
 this_path = os.path.dirname(os.path.abspath(__file__))
 lib_path = os.path.normpath(os.path.join(this_path, "bin/libs/OSX/apple_signed"))
 
-os.mkdir(source_dir)
+if not os.path.exists(source_dir):
+    os.mkdir(source_dir)
+
 for filename in os.listdir(lib_path):
     f = os.path.join(lib_path, filename)
     shutil.copy2(f, source_dir)
